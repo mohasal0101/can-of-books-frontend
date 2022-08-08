33bae4e407edb9478c8c1244ce17a75ae3b55e69
mohasal0101 <mohasal0101@gmail.com>
2022-08-08 19:46:05 +0300

    auth0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Profile from './profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-xhatj9r2.us.auth0.com"  
+      clientId="GtwvqkpJogpTd7XyLOyY8U3lchjUebli"
+      redirectUri={window.location.origin}
+    >
+     <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </Auth0Provider>
   </React.StrictMode>
 );
